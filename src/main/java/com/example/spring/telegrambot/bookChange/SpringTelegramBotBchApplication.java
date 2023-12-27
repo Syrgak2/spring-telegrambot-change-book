c74f7dee13c24e610a86198651d9d61fb4952e79
Syrgak <Kary6ekov.s@gmail.com>
2023-12-27 19:42:59 +0100

    create start bot

---
+++ src/main/java/com/example/spring/telegrambot/bookChange/SpringTelegramBotBchApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.telegrambot.bookChange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegramBotBchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTelegramBotBchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/telegrambot/bookChange/SpringTelegramBotBchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.telegrambot.bookChange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramBotBchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
